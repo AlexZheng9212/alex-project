4e010c82d73e9ca787380b07c542c59a2857a812
alex-airwallex <alex.zheng@airwallex.com>
2020-04-17 00:44:27 +0800

    leetocde 700 701

---
+++ interview/src/com/interview/leetcode/Solution700.java
@@ -0,0 +1,17 @@
+package com.interview.leetcode;
+
+import com.interview.leetcode.datastruct.TreeNode;
+
+public class Solution700 {
+  public TreeNode searchBST(TreeNode root, int val) {
+    if (root == null)
+      return null;
+    if (root.val == val)
+      return root;
+    if (root.val < val)
+      return searchBST(root.right, val);
+    if (root.val > val)
+      return searchBST(root.left, val);
+    return null;
+  }
+}
---
+++ interview/src/com/interview/leetcode/datastruct/TreeNode.java
@@ -0,0 +1,11 @@
+package com.interview.leetcode.datastruct;
+
+public class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	public TreeNode(int x) {
+		val = x;
+	};
+}
