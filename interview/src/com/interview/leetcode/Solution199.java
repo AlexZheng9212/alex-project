10ba4048f9946fe2e7e58a13998006872adaca1a
alex zheng <alex.zheng@airwallex.com>
2020-05-04 16:11:41 +0800

    leetcode..

---
+++ interview/src/com/interview/leetcode/Solution199.java
@@ -0,0 +1,30 @@
+package com.interview.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.interview.leetcode.datastruct.TreeNode;
+
+public class Solution199 {
+  private List<Integer> res = new ArrayList<>();
+
+  public List<Integer> rightSideView(TreeNode root) {
+    HashMap<Integer, Integer> hmap = new HashMap<>();
+    int level = 0;
+    helper(root, hmap, level);
+    for (int item : hmap.values()) {
+      res.add(item);
+    }
+    return res;
+  }
+
+  private void helper(TreeNode node, HashMap<Integer, Integer> hmap, int level) {
+    if (node == null)
+      return;
+    if (!hmap.containsKey(level))
+      hmap.put(level, node.val);
+    helper(node.right, hmap, level + 1);
+    helper(node.left, hmap, level + 1);
+  }
+}
---
+++ interview/src/com/interview/leetcode/Solution383.java
@@ -0,0 +1,17 @@
+package com.interview.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution383 {
+  public boolean canConstruct(String ransomNote, String magazine) {
+    Deque<Character> deque = new LinkedList<>();
+    for (char r : ransomNote.toCharArray()) {
+      deque.push(r);
+    }
+    for (char m : magazine.toCharArray()) {
+      deque.remove(m);
+    }
+    return deque.isEmpty();
+  }
+}
