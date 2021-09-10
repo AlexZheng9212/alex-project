b3169502e39fa24e76c46d22aba5e616b15b28b0
AlexZheng9212 <alex.zheng1219@gmail.com>
2021-09-10 10:24:24 +0800

    dp day 2

--- interview/src/com/interview/leetcode/solution/Solution746.java
+++ interview/src/com/interview/leetcode/solution/Solution746.java
@@ -9,6 +9,25 @@ class Solution746 {
         for (int i = 2; i < len; i++) {
             dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
         }
-        return Math.min(dp[len - 2], dp[len - 3]);
+        return Math.min(dp[len - 1], dp[len - 2]);
+    }
+
+    /**
+     * top down
+     */
+    public int minCostClimbingStairsV2(int[] cost) {
+        int len = cost.length;
+        int[] dp = new int[len];
+        dp[len - 1] = cost[len - 1];
+        dp[len - 2] = cost[len - 2];
+        for (int i = len - 3; i >= 0; i--) {
+            dp[i] = Math.min(dp[i + 1], dp[i + 2]) + cost[i];
+        }
+        return Math.min(dp[0], dp[1]);
+    }
+
+    public static void main(String[] args) {
+        Solution746 solution746 = new Solution746();
+        System.out.println(solution746.minCostClimbingStairsV2(new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }));
     }
 }
