2fb35eab5ad3c2ad794955e3abcc531baac86a2b
AlexZheng9212 <alex.zheng1219@gmail.com>
2022-01-17 23:25:09 +0800

    313

--- interview/src/com/interview/leetcode/solution/Solution313.java
+++ interview/src/com/interview/leetcode/solution/Solution313.java
@@ -1,6 +1,8 @@
 package com.interview.leetcode.solution;
 // package com.interview.leetcode;
 
+import java.util.Arrays;
+
 // class Solution313 {
 // public int nthSuperUglyNumber(int n, int[] primes) {
 // int item = 0;
@@ -16,3 +18,26 @@ package com.interview.leetcode.solution;
 // System.out.println(s.nthSuperUglyNumber(n, primes));
 // }
 // }
+
+public class Solution313 {
+  public int nthSuperUglyNumber(int n, int[] primes) {
+    int[] uglyNum = new int[n];
+    Arrays.fill(uglyNum, 0);
+    uglyNum[0] = 1;
+    int[] pointer = new int[primes.length];
+    Arrays.fill(pointer, 0);
+    for (int i = 1; i < n; i++) {
+      int tmp = Integer.MAX_VALUE;
+      for (int j = 0; j < primes.length; j++) {
+        tmp = Math.min(tmp, uglyNum[pointer[j]] * primes[j]);
+      }
+      uglyNum[i] = tmp;
+      for (int j = 0; j < primes.length; j++) {
+        if (uglyNum[pointer[j]] * primes[j] == tmp) {
+          pointer[j]++;
+        }
+      }
+    }
+    return uglyNum[n - 1];
+  }
+}
